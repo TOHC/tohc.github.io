717a285927958c41d4c5255d4d3086cfc4b293a5
pstakoun <peter_st@live.com>
2016-06-18 22:05:15 -0400

    TH update

--- thacks/script.js
+++ thacks/script.js
@@ -1,6 +1,31 @@
 function colorCursor(str)
 {
-	document.querySelectorAll(".intro-content .intro-text .cursor")[0].className = "cursor " + str;
+	document.querySelectorAll(".content .intro-text .cursor")[0].className = "cursor " + str;
+}
+
+var s = 0;
+
+function slideIntroText()
+{
+	if (parseInt(document.querySelectorAll(".content .buffer")[0].style.height) < 1) {
+		document.querySelectorAll(".content .about-text")[0].className = document.querySelectorAll(".content .about-text")[0].className.replace(/\bhidden\b/,'');
+		setTimeout(function(){document.querySelectorAll(".content .signup-form")[0].className = document.querySelectorAll(".content .signup-form")[0].className.replace(/\bhidden\b/,'')}, 1500);
+		return;
+	}
+	document.querySelectorAll(".content .buffer")[0].style.height = parseInt(document.querySelectorAll(".content .buffer")[0].style.height) - s + "px";
+	setTimeout(slideIntroText, 1);
+}
+
+function showColor2Text(str)
+{
+	if (str.length == 0) {
+		document.querySelectorAll(".content .intro-text .cursor")[0].style.visibility = "hidden";
+		slideIntroText();
+		return;
+	}
+	document.querySelectorAll(".content .intro-text .title.color2")[0].innerHTML = document.querySelectorAll(".content .intro-text .title.color2")[0].innerHTML + str.charAt(0);
+	colorCursor("color2");
+	setTimeout(function(){showColor2Text(str.substring(1))}, 200);
 }
 
 function showColor1Text(str)
@@ -9,19 +34,14 @@ function showColor1Text(str)
 		showColor2Text("Hacks");
 		return;
 	}
-	document.querySelectorAll(".intro-content .intro-text .title.color1")[0].innerHTML = document.querySelectorAll(".intro-content .intro-text .title.color1")[0].innerHTML + str.charAt(0);
+	document.querySelectorAll(".content .intro-text .title.color1")[0].innerHTML = document.querySelectorAll(".content .intro-text .title.color1")[0].innerHTML + str.charAt(0);
 	colorCursor("color1");
-	setTimeout(function(){showColor1Text(str.substring(1))}, 250);
+	setTimeout(function(){showColor1Text(str.substring(1))}, 200);
 }
 
-function showColor2Text(str)
-{
-	if (str.length == 0) {
-		return;
-	}
-	document.querySelectorAll(".intro-content .intro-text .title.color2")[0].innerHTML = document.querySelectorAll(".intro-content .intro-text .title.color2")[0].innerHTML + str.charAt(0);
-	colorCursor("color2");
-	setTimeout(function(){showColor2Text(str.substring(1))}, 250);
-}
-
-window.onload = function(){document.querySelectorAll(".intro-content .intro-text .cursor")[0].style.visibility="visible";setTimeout(function(){showColor1Text("T.")}, 250)};
+window.onload = function() {
+	s = (document.querySelectorAll(".content")[0].offsetHeight - document.querySelectorAll(".content .intro-text")[0].offsetHeight) / 500;
+	document.querySelectorAll(".content .buffer")[0].style.height = document.querySelectorAll(".content")[0].offsetHeight - document.querySelectorAll(".content .intro-text")[0].offsetHeight + "px";
+	document.querySelectorAll(".content .intro-text .cursor")[0].style.visibility = "visible";
+	setTimeout(function(){showColor1Text("T.")}, 200);
+};
